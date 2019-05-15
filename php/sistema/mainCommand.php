87786aa9bad10e422cf54ab8ec5f4aa58ef1d6cf
Carlos <carlorregod@gmail.com>
2019-05-15 09:04:06 -0400

    CRUD Completo

--- php/sistema/mainCommand.php
+++ php/sistema/mainCommand.php
@@ -2,25 +2,55 @@
 require "../conexion.php";
 
 $cmd = $_POST['cmd'];
-
+$id = $_POST['id'];
 
 switch($cmd)
 {
     case "CargaTabla":
         $pgsql = getConnect();
-        $query = "SELECT * FROM articulos1";
+        $query = "SELECT * FROM articulos1 ORDER BY serie";
         $result = pg_query($pgsql, $query) or die("Problemas con la consulta");
-
         $lista="";
         while($fila = pg_fetch_object($result))
         {
             $lista .= '<tr><th>'.$fila->serie.'</th><td>'.$fila->producto.
-            '</td><td>BOTON EDITAR</td><td>BOTON BORRAR</td></tr>';
+            '</td><td><button id="'.$fila->id.'" class="btn btn-outline-primary" onclick="editar(this)">Editar</button>      
+            </td><td><button class="btn btn-outline-danger btnBorrado" id="'.$fila->id.'">Eliminar</button></td></tr>';
         }
         pg_close($pgsql);
         echo $lista;
         break;
-    
+        
+    case "BorrarRegistro":       
+        $pgsql = getConnect();
+        $query = "DELETE FROM articulos1 WHERE id='$id'";
+        $result = pg_query($pgsql, $query) or die("Problemas con la consulta");
+        pg_close($pgsql);
+        echo "";
+        break;
+        
+    case "EditarRegistro":
+        $pgsql = getConnect();
+        $query = "SELECT * FROM articulos1 WHERE id='$id' LIMIT 1";
+        $result = pg_query($pgsql, $query) or die("Problemas con la consulta");
+        $resultado = pg_fetch_array($result);
+        pg_close($pgsql);
+        echo json_encode($resultado, JSON_FORCE_OBJECT);
+        break;
+        
+    case "ActualizarRegistro":
+        $producto = $_POST['producto'];
+        $pgsql = getConnect();
+        $query = "UPDATE articulos1 SET producto='$producto' WHERE id='$id'";
+        $result = pg_query($pgsql, $query) or die("Problemas con la consulta");
+        $query = "SELECT * FROM articulos1 WHERE id='$id' LIMIT 1";
+        unset($result);
+        $result = pg_query($pgsql, $query) or die("Problemas con la consulta");
+        $resultado = pg_fetch_array($result);
+        pg_close($pgsql);
+        echo json_encode($resultado, JSON_FORCE_OBJECT);
+        break;
+        
     default:
         break;
 }
--- sistema/main.php
+++ sistema/main.php
@@ -13,6 +13,37 @@ mantener_sesion();
         <title>Bienvenido</title>
     </head>
     <body>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Editar registro:&nbsp </h5>
+            <h5 class="modal-title" id="registroCustomID"></h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+           <table>
+           	<tr>
+           		<td>Nombre nuevo:</td>
+           		<td><input type="text" id="txtNombreProducto" value="" placeholder="Ingrese nombre"/></td>
+           	</tr>
+           </table>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+            <button type="button" class="btn btn-primary" id="txtIdProducto" onclick="actualizar(this)">Actualizar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+    
+    <!-- Formulario -->
        	<h2 class="p-3 mb-2 bg-primary text-white">Bienvenido al sistema <?php echo strtoupper($_SESSION['nombre']);?></h2>
         <table class="table table-sm px-4 py-4">
               <thead>
@@ -28,15 +59,17 @@ mantener_sesion();
               </tbody>
         </table>
        	<form action="../php/login/logout.php" method="POST">
-       		<input type="submit" class="btn btn-primary my-2 mx-5" value="Salir">
+       		<input type="submit" class="btn btn-primary btn-block my-2 mx-2" value="Salir">
        	</form>
 
-   		<button class="btn btn-primary my-2 mx-5" id="btnSalir" onclick="salida()">Salir por js</button>
+   		<button class="btn btn-primary btn-block my-2 mx-2" id="btnSalir" onclick="salida()">Salir por js</button>
     </body>
     <footer>
     	<script src="https://code.jquery.com/jquery-3.4.0.js"
 			  integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo="
 			  crossorigin="anonymous"></script> 
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 		<script src="../js/utilidades.js"></script>
 		<script src="../js/sistema/main.js"></script>	 
 		<script src="../js/logout.js"></script>	  	  
